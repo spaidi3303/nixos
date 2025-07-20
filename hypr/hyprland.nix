5d8207e1f641346e9d0b9f967fe1dd25d814be9d
sammy <lednevdosa@gmail.com>
2025-07-20 12:39:15 +0300

    asd

--- hypr/hyprland.nix
+++ hypr/hyprland.nix
@@ -2,20 +2,25 @@
 
 let
   hyprConf = ./hyprland.conf;
-in
-{
-  programs.hyprland = {
+in {
+  services.xserver.enable = true;
+
+  services.xserver.windowManager.hyprland = {
     enable = true;
-    withUWSM = true;
+    package = pkgs.hyprland;
   };
 
-  home.file.".config/hypr/hyprland.conf".source = hyprConf;
-
-  home.packages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     hyprland
     hyprpaper
     kitty
     hyprshot
     hyprpanel
-    ];
+  ];
+
+  system.activationScripts.copyHyprlandConfig.text = ''
+    mkdir -p /home/sammy/.config/hypr
+    cp ${toString hyprConf} /home/sammy/.config/hypr/hyprland.conf
+    chown -R sammy:sammy /home/sammy/.config/hypr
+  '';
 }
