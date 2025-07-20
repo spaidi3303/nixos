e1c2519616d6fdeffd08d0a7e756e25dbcf86718
sammy <lednevdosa@gmail.com>
2025-07-20 18:02:59 +0300

    home.nix

--- home.nix
+++ home.nix
@@ -6,11 +6,6 @@
     username = "sammy";
     homeDirectory = "/home/sammy";
     stateVersion = "25.05";
-    activation.cloneDotfiles = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
-      if [ ! -d "$HOME/hypr" ]; then
-        ${pkgs.git}/bin/git clone https://github.com/spaidi3303/hypr.git "$HOME/hypr"
-      fi
-    '';
     enableNixpkgsReleaseCheck = false;
   };
 
