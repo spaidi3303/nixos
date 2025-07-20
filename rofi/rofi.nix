bcd0a24c613f31e3c9c41acc38d91ee33d499e55
sammy <lednevdosa@gmail.com>
2025-07-20 13:18:07 +0300

    rofi.nix

--- rofi/rofi.nix
+++ rofi/rofi.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
 
+let
+  rofiFiles = ./files;
+in
+{
   environment.systemPackages = with pkgs; [
     rofi
   ];
 
   system.activationScripts.copyRofiFiles.text = ''
     mkdir -p /home/sammy/.config/rofi
-    cp -r ./files/* /home/sammy/.config/rofi/
+    cp -r ${toString rofiFiles}/* /home/sammy/.config/rofi/
+    chown -R sammy:users /home/sammy/.config/rofi
   '';
 }
