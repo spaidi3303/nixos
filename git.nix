02047830b517a7091a34f6f4b8a390437f3f8a88
sammy <lednevdosa@gmail.com>
2025-07-20 13:29:39 +0300

    git.nix

--- git.nix
+++ git.nix
@@ -2,8 +2,6 @@
     programs = {
         git = {
             enable = true;
-            userName = "sammy";
-            userEmail = "lednevdosa@gmail.com";
         };
     };
     environment.systemPackages = with pkgs; [
