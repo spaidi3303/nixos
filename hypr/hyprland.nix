f0cd1adff02a69ca662fb66051a881098045251b
sammy <lednevdosa@gmail.com>
2025-07-21 16:21:59 +0300

    fish.nix

--- hypr/hyprland.nix
+++ hypr/hyprland.nix
@@ -15,15 +15,5 @@ in {
     hyprpanel
   ];
 
-  system.activationScripts.copyHyprlandConfig.text = ''
-    unlink /home/sammy/.config/hypr/hyprland.conf
-    mkdir -p /home/sammy/.config/hypr
-    cp ${toString hyprConf} /home/sammy/.config/hypr/hyprland.conf
-  '';
 
-  system.activationScripts.copyHyprpaperConfig.text = ''
-    unlink /home/sammy/.config/hypr/hyprpaper.conf
-    cp ${toString hyprpaperConf} /home/sammy/.config/hypr/hyprpaper.conf
-    chown -R sammy:users /home/sammy/.config/hypr
-  '';
 }
