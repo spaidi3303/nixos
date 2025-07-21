21a1148bd2ad0a8d3cf77dde71d8e708086be756
sammy <lednevdosa@gmail.com>
2025-07-21 17:38:15 +0300

    hypr

--- hypr/hyprland.nix
+++ hypr/hyprland.nix
@@ -14,6 +14,16 @@ in {
     hyprshot
     hyprpanel
   ];
+  system.activationScripts.copyHyprlandConfig.text = ''
+    unlink /home/sammy/.config/hypr/hyprland.conf
+    mkdir -p /home/sammy/.config/hypr
+    cp ${toString hyprConf} /home/sammy/.config/hypr/hyprland.conf
+  '';
 
+  system.activationScripts.copyHyprpaperConfig.text = ''
+    unlink /home/sammy/.config/hypr/hyprpaper.conf
+    cp ${toString hyprpaperConf} /home/sammy/.config/hypr/hyprpaper.conf
+    chown -R sammy:users /home/sammy/.config/hypr
+  '';
 
 }
