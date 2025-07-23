9496764be7b934fdb4876637953b16e2606bc9b3
sammy <lednevdosa@gmail.com>
2025-07-23 14:06:53 +0300

    swww

--- hypr/hyprland.nix
+++ hypr/hyprland.nix
@@ -2,15 +2,12 @@
 
 let
   hyprConf = ./hyprland.conf;
-  hyprpaperConf = ./hyprpaper.conf;
-  pyprConf = ./pypr;
 in {
 
   programs.hyprland.enable = true;
 
   environment.systemPackages = with pkgs; [
     hyprland
-    hyprpaper
     hyprshot
     hyprpanel
   ];
@@ -18,11 +15,6 @@ in {
     unlink /home/sammy/.config/hypr/hyprland.conf
     mkdir -p /home/sammy/.config/hypr
     cp ${toString hyprConf} /home/sammy/.config/hypr/hyprland.conf
-  '';
-
-  system.activationScripts.copyHyprpaperConfig.text = ''
-    unlink /home/sammy/.config/hypr/hyprpaper.conf
-    cp ${toString hyprpaperConf} /home/sammy/.config/hypr/hyprpaper.conf
     chown -R sammy:users /home/sammy/.config/hypr
   '';
 
